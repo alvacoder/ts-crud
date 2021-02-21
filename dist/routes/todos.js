37e263ad435565fbdee5b8e9e0d4abd0277e4491
alvacoder <idrisadeniji01@gmail.com>
2021-02-20 20:55:16 -0500

    remove dist from repo

--- dist/routes/todos.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const router = express_1.Router();
-router.post('/');
-router.get('/');
-router.patch('/:id');
-router.delete('/:id');
-exports.default = router;
