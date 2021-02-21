dd5072d7b36fb80166a4da3d2be9874064c9c9a1
alvacoder <idrisadeniji01@gmail.com>
2021-02-20 20:50:00 -0500

    handle invalid request/endpoints

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
-import express from 'express';
+import express, {Request, Response, NextFunction} from 'express';
+import todoRoutes from './routes/todos';
 
 const app = express();
+
+app.use('/todos', todoRoutes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    res.status(404).send({responseCode: 404, status: 'failure', message: 'Invalid resource url', data: []});
+});
 
 app.listen(3000);
