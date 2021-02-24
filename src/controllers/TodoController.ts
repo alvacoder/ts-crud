c53272550e8e9d3b1504dc7d4971822cb75d0544
alvacoder <idrisadeniji01@gmail.com>
2021-02-23 20:09:40 -0500

    create todo controller

---
+++ src/controllers/TodoController.ts
@@ -0,0 +1,11 @@
+import { RequestHandler } from 'express';
+import { Todo } from '../models/todo';
+const todos: Todo[] = [];
+
+export const createTodo: RequestHandler = (req, res, next) => {
+    const text = (req.body as {text: string}).text;
+    const newTodo = new Todo(Math.random().toString(), text);
+    todos.push(newTodo);
+
+    res.status(201).send({message: 'created new todo', data: newTodo});
+}
